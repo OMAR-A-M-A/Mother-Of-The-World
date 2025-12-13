6e899f800a7117823d721475b06e4770b6c4c5d6
OMAR-A-M-A <oa5252842@gmail.com>
2025-12-13 02:59:28 +0200

    add navbar & hero-section

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg bg-white sticky-top">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="#">
+            <div class="logo-icon me-2">
+                <img src="assets/images/motw-logo.png" alt="" width="40">
+            </div>
+            <div class="logo-text">
+                <span class="text-travel">MOTHER</span><span class="text-tour">WORLD</span>
+            </div>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto align-items-center">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Category</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tour Places</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Governorates</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -3,11 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <title>Mother Of The World</title>
 </head>
+
 <body>
-    <h1>
-        home page
-    </h1>
+    <?php include 'includes/header.php'; ?>
+    <main>
+        <!------------ hero section ------------>
+        <section class="hero-section">
+        <div class="container hero-content">
+            <h1 class="hero-title">Find Next Place To Visit</h1>
+            <p class="hero-subtitle">Discover amazing places at exclusive deals</p>
+        </div>
+        </section>
+        <!------------ info section ------------>
+        <section class="info-bar-container">
+            <div class="info-bar">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="feature-item">
+                        <div class="feature-icon">
+                            <i class="fa-solid fa-earth-americas"></i>
+                        </div>
+                        <div class="feature-text">
+                            <h5>700+ Destinations</h5>
+                            <p>Our expert team handpicked all destinations in this site</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="feature-item">
+                        <div class="feature-icon">
+                            <i class="fa-solid fa-tags"></i> </div>
+                        <div class="feature-text">
+                            <h5>Best Price Guarantee</h5>
+                            <p>Price match within 48 hours of order confirmation</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="feature-item">
+                        <div class="feature-icon">
+                            <i class="fa-solid fa-headset"></i> </div>
+                        <div class="feature-text">
+                            <h5>Top Notch Support</h5>
+                            <p>We are here to help, before, during, and even after your trip.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        </section>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </main>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
