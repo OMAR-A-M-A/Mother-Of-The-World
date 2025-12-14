9854749c1eb4048c5a45b8b2cd104bf1a2e6a2a0
FatmaElghoury <fatmaelghoury@gmail.com>
2025-12-14 20:15:08 +0200

    second commit

--- gov.php
+++ gov.php
@@ -1,11 +1,43 @@
+<?php
+include "includes/db_connect.php";
+if (!$conn) {
+    die("DB not connected");
+}
+
+
+$query = "SELECT * FROM governorates";
+$result = $conn->query($query);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Governorates</title>
+    <link rel="stylesheet" href="assets/css/gov.css">
 </head>
 <body>
-    hellow
+
+<h1 class="page-title">Egypt Governorates</h1>
+
+<div class="cards-container">
+    <?php while($row = mysqli_fetch_assoc($result)) { ?>
+        <div class="card">
+           <img src="assets/uploads/governorates/<?php echo $row['image_url']; ?>" 
+                alt="<?php echo $row['G_name']; ?>">
+            <div class="card-content">
+                <h3><?php echo $row['G_name']; ?></h3>
+                <p>
+                    <?php echo substr($row['G_description'], 0, 120); ?>...
+                </p>
+                <a href="gov-details.php?id=<?php echo $row['G_ID']; ?>" class="btn">
+                    View Details
+                </a>
+            </div>
+        </div>
+    <?php } ?>
+</div>
+
+
 </body>
 </html>
